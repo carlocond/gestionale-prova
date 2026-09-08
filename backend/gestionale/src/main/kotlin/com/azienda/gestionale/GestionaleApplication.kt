206f69814cbacabdf523052d8e3198478d05dbdf
Carlo Condello <carlocondello05@gmail.com>
2026-09-08 09:37:19 +0200

    Initial project scaffold backend + android-app

---
+++ backend/gestionale/src/main/kotlin/com/azienda/gestionale/GestionaleApplication.kt
@@ -0,0 +1,11 @@
+package com.azienda.gestionale
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class GestionaleApplication
+
+fun main(args: Array<String>) {
+	runApplication<GestionaleApplication>(*args)
+}
